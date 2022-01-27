206ecc2f065f0ce4fcca06431fe63101350ea1ba
NightMarmot <92267504+NightMarmot@users.noreply.github.com>
2022-01-27 17:29:47 +0300

    Add files via upload

---
+++ News/templatetags/custom_filters.py
@@ -0,0 +1,30 @@
+from django import template
+import re
+import random
+
+
+register = template.Library()
+
+STOP_LIST = [
+    'жопа',
+    'хуй',
+    'пиздец',
+]
+
+SYMBOLS = ['*', '!', '#']
+
+
+@register.filter(name='censor')
+def censor(text: str):
+    words = text.split(' ')
+    new_words = []
+
+    for i, word in enumerate(words):
+        match = re.match(f"^{'|'.join(STOP_LIST)}", word)
+
+        if match is not None:
+            new_words.append(''.join([random.choice(SYMBOLS)] * len(word)))
+        else:
+            new_words.append(word)
+
+    return ' '.join(new_words)
