d97738c5094c4fa2d92373ad4dff7e6a076a4a3e
NightMarmot <92267504+NightMarmot@users.noreply.github.com>
2022-01-27 18:05:49 +0300

    Add files via upload

--- News/views.py
+++ News/views.py
@@ -10,7 +10,13 @@ class PostList(ListView):
     context_object_name = 'news'
     ordering = ['-publish']
     paginate_by = 10
-    queryset = Post.objects.order_by('-publish')
+    queryset = Post.objects.order_by('-id')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['value1'] = None
+        return context
 
 
 class PostDetail(DetailView):
