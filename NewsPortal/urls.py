1d955b638d34bff9259cac38f4e075d125920c27
NightMarmot <92267504+NightMarmot@users.noreply.github.com>
2022-02-03 17:16:12 +0300

    Add files via upload

--- NewsPortal/urls.py
+++ NewsPortal/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from News.views import IndexView
+
 
 urlpatterns = [
+    path('', IndexView.as_view()),
     path('admin/', admin.site.urls),
+    path('', include('protect.urls')),
+    path('pages/', include('django.contrib.flatpages.urls')),
+    path('post/', include('News.urls')),
+    path('account/', include('allauth.urls')),
+    path('sign/', include('sign.urls')),
 ]
